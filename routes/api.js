5f6217c8d2710a8db63a96d857f98dc97c7b0989
simon-jk-casey <jesusiwasevil@gmail.com>
2017-01-31 12:41:14 +1300

    created routes

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const passport = require('../passportSetup');
 
+//ADD IN PASSPORT AUTH TO REQUIRED ROUTES
+
 //get players
 router.get('/players', (req, res) => {})
 //add player
@@ -16,5 +18,34 @@ router.get('/fixtures', (req, res) => {})
 //add fixtures
 router.post('/fixtures', (req, res) => {})
 //get fixture by id
+router.get('/fixtures/:id', (req, res) => {})
+//authenticated/authorised user can edit fixture details
+router.post('/fixtures/:id', (req, res) => {})
+//show results
+router.get('/results', (req, res) => {})
+//add result
+router.post('/results', (req, res) => {})
+//show details(scorecard) of specific result by id
+router.get('/results/:id', (req, res) => {})
+//add batting scorecard
+router.post('/batCard'), (req, res) => {})
+//add bowling scorecard
+router.post('/bowlCard'), (req, res) => {})
+//add fielding scorecard
+router.post('/fieldCard'), (req, res) => {})
+//show mvp points
+router.get('/mvp', (req, res) => {})
+//add mvp points
+router.post('/mvp', (req, res) => {})
+
+//stretch routes - fielding planner
+//get fielding positions
+router.get('/fieldPositions', (req, res) => {})
+//save custom fieldPositions
+router.post('/customFields', (req, res) => {})
+//show saved custom fieldPositions
+router.get('customFields', (req, res) => {})
+
+
 
 module.exports = router
